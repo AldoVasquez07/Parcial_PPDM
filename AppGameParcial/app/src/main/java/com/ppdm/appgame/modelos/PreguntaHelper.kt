f7c7e5bf630774311b00199ff9d0d075b5e7ebd2
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-10-19 10:00:17 -0500

    Creando la clase PreguntaHelper, la cual es de tipo object para garantizar que no se instancie, y obtener las preguntas establecidas.

---
+++ AppGameParcial/app/src/main/java/com/ppdm/appgame/modelos/PreguntaHelper.kt
@@ -0,0 +1,10 @@
+package com.ppdm.appgame.modelos
+
+object PreguntaHelper {
+    private val preguntas = listOf(
+        Pregunta("¿Cuál es la capital de Francia?", listOf("París", "Londres", "Madrid", "Arequipa"), "París"),
+        Pregunta("¿Cuál es el planeta más grande?", listOf("Marte", "Tierra", "Júpiter", "Urano"), "Júpiter"),
+        Pregunta("¿Cuándo se descubrió América?", listOf("1491", "1493", "2025", "1942"), "1942"),
+        Pregunta("¿Cuánto es: 2 * 500 - 40 / 2?", listOf("1020", "980", "2000", "970"), "980")
+    )
+}
