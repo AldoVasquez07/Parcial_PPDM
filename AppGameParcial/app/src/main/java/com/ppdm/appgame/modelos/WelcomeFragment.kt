700fb262853921e2d96bb4217ff160e8499f7eed
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-10-19 11:30:01 -0500

    Agregando un nav_graph, para la navegación entre fragments. Además, se ha conectado la clase WelcomeFragment con su vista fragment_welcome

--- AppGameParcial/app/src/main/java/com/ppdm/appgame/modelos/WelcomeFragment.kt
+++ AppGameParcial/app/src/main/java/com/ppdm/appgame/modelos/WelcomeFragment.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.ppdm.appgame.R
 
 class WelcomeFragment: Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+        val startButton: Button = view.findViewById(R.id.startButton)
+        /*startButton.setOnClickListener{
+            findNavController().navigate
+        }*/
         return view
     }
 }
