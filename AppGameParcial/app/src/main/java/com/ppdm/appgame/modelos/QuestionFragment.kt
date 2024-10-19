2b6865a8aa7506e3b9e9ff56416b5105ff992f73
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-10-19 11:38:31 -0500

    Añadiendo la conexión en la clase QuestionFragment con su vista: fragment_question.xml

--- AppGameParcial/app/src/main/java/com/ppdm/appgame/modelos/QuestionFragment.kt
+++ AppGameParcial/app/src/main/java/com/ppdm/appgame/modelos/QuestionFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.ppdm.appgame.R
 
 class QuestionFragment: Fragment() {
     private val preguntas = PreguntaHelper.preguntas
@@ -14,6 +15,12 @@ class QuestionFragment: Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_question, container, false)
+        displayQuestion(view)
         return view
+    }
+
+    private fun displayQuestion(view: View){
+        
     }
 }
