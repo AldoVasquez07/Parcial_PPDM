cac07ce6705992999536f0cfeb66dec086618dc5
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-10-19 11:33:05 -0500

    Agregando conexión entre WelcomeFragment y QuestionFragment

--- AppGameParcial/app/src/main/java/com/ppdm/appgame/modelos/WelcomeFragment.kt
+++ AppGameParcial/app/src/main/java/com/ppdm/appgame/modelos/WelcomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.ppdm.appgame.R
 
 class WelcomeFragment: Fragment() {
@@ -15,9 +16,9 @@ class WelcomeFragment: Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_welcome, container, false)
         val startButton: Button = view.findViewById(R.id.startButton)
-        /*startButton.setOnClickListener{
-            findNavController().navigate
-        }*/
+        startButton.setOnClickListener{
+            findNavController().navigate(R.id.action_welcomeFragment_to_questionFragment)
+        }
         return view
     }
 }
