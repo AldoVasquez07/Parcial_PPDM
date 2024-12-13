bc98c140d12b70596b5b0ba8acd6262a287adcc1
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2024-12-12 23:22:27 -0500

    Agregando Comentarios en AnswerFragment

--- AppGameParcial/app/src/main/java/com/ppdm/appgame/modelos/AnswerFragment.kt
+++ AppGameParcial/app/src/main/java/com/ppdm/appgame/modelos/AnswerFragment.kt
@@ -16,23 +16,33 @@ class AnswerFragment: Fragment() {
         saveInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_answer, container, false)
+        // Obteniendo los argumentos pasados entre fragments
         val args = AnswerFragmentArgs.fromBundle(requireArguments())
 
+        // Obteniendo TextoView para mostrar el mensaje de haber respondido
         val feedbackText: TextView = view.findViewById(R.id.feedbackText)
 
+        // Consultando si la pregunta se ha respondido bien
         if (args.textView) {
+            // Actualizando mensaje
             feedbackText.text = "¡Respuesta Correcta! Premio: 10 COINS"
+            // Aumentando cantidad de monedas 
             PerfilHelper.perfil.monedas += 10
         }
         else {
+            // Mostrando que se respondió incorrectamente
             feedbackText.text = "Respuesta Incorrecta"
         }
+        
         val nextButton: Button = view.findViewById(R.id.nextButton)
         nextButton.setOnClickListener{
+            // Preguntando si aún hay preguntas
             if (PreguntaHelper.index < PreguntaHelper.preguntas.size){
+                // Redirigir a la pregunta
                 findNavController().navigate(R.id.action_answerFragment_to_questionFragment)
             }
             else {
+                // Reestablecer indice a 0 para seguir mostrando preguntas            
                 PreguntaHelper.index=0
                 findNavController().navigate(R.id.action_answerFragment_to_finalFragment)
             }
