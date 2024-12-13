ae92ba098a0adada23a5ce5af4b0b0a66923aca7
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2024-12-12 23:52:59 -0500

    Comentando WelcomeFragment

--- AppGameParcial/app/src/main/java/com/ppdm/appgame/modelos/WelcomeFragment.kt
+++ AppGameParcial/app/src/main/java/com/ppdm/appgame/modelos/WelcomeFragment.kt
@@ -16,22 +16,26 @@ class WelcomeFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_welcome, container, false)
-
+        // Estableciendo nombre del usuairo
         val welcomeText: TextView = view.findViewById(R.id.welcomeText)
         welcomeText.text = PerfilHelper.perfil.nombre
 
+        // Estableciendo las monedas del usuario
         val coins: TextView = view.findViewById(R.id.coin_count)
         coins.text = PerfilHelper.perfil.monedas.toString()
 
+        // Estableciendo puntaje del usuario
         val points: TextView = view.findViewById(R.id.puntaje)
         points.text = PerfilHelper.perfil.puntaje.toString()
 
         val startButton: Button = view.findViewById(R.id.startButton)
         startButton.setOnClickListener{
+            // Pasar al aspecto del formulario si se da click al botón para jugar
             findNavController().navigate(R.id.action_welcomeFragment_to_questionFragment)
         }
         val rankingButton: Button = view.findViewById(R.id.rankingButton)
         rankingButton.setOnClickListener {
+            // Pasar al aspecto del ranking si se da click al botón ranking
             findNavController().navigate(R.id.action_welcomeFragment_to_rankingFragment)
         }
         return view
